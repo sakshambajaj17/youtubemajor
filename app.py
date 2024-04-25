04ec7b590c094fe1e39ea2826d058b45d1013ee2
Saksham Bajaj <sakshambajaj17@gmail.com>
2024-04-25 12:48:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv() ##load all the nevironment variables
 import os
-import generativeai as genai
+import google.generativeai as genai
 
 from youtube_transcript_api import YouTubeTranscriptApi
 
